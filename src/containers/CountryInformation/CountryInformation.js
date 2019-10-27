47be4c946a1470bbade9d916d6a409683757c13e
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-10-27 13:08:52 +0600

    The stateful component CountryInformation is created. Axios is imported to be used for API requests.

---
+++ src/containers/CountryInformation/CountryInformation.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import axios from "axios";
+import './CountryInformation.css';
+
+class CountryInformation extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default CountryInformation;
