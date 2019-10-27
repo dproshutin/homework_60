6fc29661555e843ddfc26b93db77e9fdd5f01aa8
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-10-27 13:57:44 +0600

    Axios returns country information as an array. Each array item is a object with country's code as id and country name. ComponentDidMount updates the country information in the state.

--- src/containers/CountryInformation/CountryInformation.js
+++ src/containers/CountryInformation/CountryInformation.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import Country from "../../components/Country/Country";
 import './CountryInformation.css';
 
 class CountryInformation extends Component {
+    state = {
+        countries: []
+    };
+    componentDidMount() {
+        const BASE_URL = "https://restcountries.eu/rest/v2/all?fields=name;alpha3Code";
+        axios.get(BASE_URL).then(response => {
+            return response.data;
+        }).then(countries => {
+            const preparedCountries = countries.map(country => {
+                return {name: country.name, id: country.alpha3Code};
+            });
+            return preparedCountries;
+        }).then(countries => this.setState({countries}));
+    }
+
     render() {
+        console.log(this.state.countries);
         return (
-            <div></div>
+            <div>
+            </div>
         );
     }
 }
